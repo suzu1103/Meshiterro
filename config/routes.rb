8ca8f587491afa6933f94a41ccdd0b4bf62468d5
suzu1103 <4425krkh@gmail.com>
2022-04-10 09:15:05 +0000

    [Add]4-17

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   devise_for :users
+  get 'homes/about' => 'homes#about', as: 'about'
   
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
   
-  get 'homes/about' => 'homes#about', as: 'about'
+  
+  
+
 end
