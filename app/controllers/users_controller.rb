fbeeb706608023567e1806e1be3abf37ec98245a
suzu1103 <4425krkh@gmail.com>
2022-04-12 15:28:25 +0000

    [Add]4-22 ページネーション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images 
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
@@ -21,5 +21,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-  
+
 end
